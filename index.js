509dfbf6b768706ca68e4aaa43b20ebc176b42d9
patrick kisyula <pmusyimi42@gmail.com>
2022-11-17 05:38:45 +0300

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Code your solutions in this file
+// // Code your solutions in this file
+// // for (let age=30; age<40; age++){
+// //     console.log(`I'm ${age} years old. Happy birthday to me!`);
+// // }
+// const gifts = ['teddy bear', 'drone', 'doll']
+// function wrapGifts(gifts){
+//     for(let i = 0; i < gifts.length; i++) {
+//         console.log(`Wrapped ${gifts[i]} and added a bow!`)
+//     }
+//     return gifts;
+// }
+// wrapGifts(gifts);
+function writeCards(name, eventName){
+    const messageArray = [];
+    for(let i=0; i< name.length; i++){
+        // const msg = console.log('thankyou ' + [name[i]] + ' ' + eventName)
+        const msg = `Thank you, ${name[i]}, for the wonderful ${eventName} gift!`;
+        messageArray.push(msg);
+    }
+    return messageArray;
+};
+//  writeCards(['Charlie', 'Samip'], 'business');
+function countDown(positiveInteger){
+    while(positiveInteger >= 0){
+        console.log(positiveInteger);
+        positiveInteger--;
+        debugger;
+    }
+    return positiveInteger;
+}
+// countDown(10);
+
+
